367863b50134309ad55b5aaee054b172b7b910e4
fedeviotti <fedeviotti@gmail.com>
2019-06-12 00:10:17 +0200

    setup project

---
+++ src/test/java/hello/GreeterTest.java
@@ -0,0 +1,16 @@
+package test.java.hello;
+
+import main.java.hello.Greeter;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+public class GreeterTest {
+
+    private Greeter greeter = new Greeter();
+
+    @Test
+    public void greeterSaysHello() {
+        assertThat(greeter.sayHello(), containsString("Hello"));
+    }
+}
